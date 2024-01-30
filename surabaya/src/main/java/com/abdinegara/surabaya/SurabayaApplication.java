ab143248c9f7513fd739db113c29f0ad155eacb4
Aldiansyah <aldiansyah@iforce.co.id>
2024-01-30 23:28:46 +0700

    first commit

---
+++ surabaya/src/main/java/com/abdinegara/surabaya/SurabayaApplication.java
@@ -0,0 +1,13 @@
+package com.abdinegara.surabaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurabayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurabayaApplication.class, args);
+	}
+
+}
---
+++ surabaya/src/test/java/com/abdinegara/surabaya/SurabayaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdinegara.surabaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurabayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
